79aa8083c09a575dc1a72d5be2b0c8c9955ee683
root <root@master.example.com>
2022-06-14 04:25:57 +0000

    02-K8s/00-Setup

--- 02-K8s/00-Setup/install-k8s-worker-node.sh
+++ 02-K8s/00-Setup/install-k8s-worker-node.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-echo "Installing docker"
+echo "******************* Installing docker ********************"
+sleep 2 
 apt-get update
 apt-get install -y \
     apt-transport-https \
@@ -13,11 +14,11 @@ add-apt-repository \
    stable"
 apt-get update && apt-get install -y docker-ce=$(apt-cache madison docker-ce | grep 17.03 | head -1 | awk '{print $3}')
 
-echo "installing kubeadm and kubectl"
+echo "********************* Installing kubeadm and kubectl *****************************"
 apt-get update && apt-get install -y apt-transport-https
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
 deb http://apt.kubernetes.io/ kubernetes-xenial main
 EOF
 apt-get update
-apt-get install -y  kubelet=1.20.0-00 kubeadm=1.20.0-00 kubectl=1.20.0-00
+apt-get install -y  kubelet=1.21.0-00 kubeadm=1.21.0-00 kubectl=1.21.0-00
